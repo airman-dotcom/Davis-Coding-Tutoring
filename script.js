182d488deda4b77109892f4046fd765207d1802b
airman-dotcom <amathakbari@gmail.com>
2025-03-17 21:45:34 -0700

    testing smth

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let laptop = document.getElementById("laptop");
-const scaling = 1058.571429;
-
-window.onresize = function (){
-    laptop.style = `scale: ${window.innerWidth/scaling};` 
+window.scrollTo(0,0);
+function pxtovh(px){
+    return (px / window.innerHeight) * 100;
 }
-window.addEventListener("scroll", function () {
-    console.log(1)
-}, false);
+
+let launch = document.getElementById("launch");
+let launch_txt = "Launch into the <span style='color: blue;'>future</span>";
+
+function typewriter(i){
+    if (i == launch_txt.length) return;
+    if (launch_txt[i] == "<"){
+        setTimeout(function(){
+            launch.innerHTML += launch_txt.slice(i, launch_txt.length);
+        }, 700)
+        return;
+    }
+    launch.innerHTML += launch_txt.charAt(i)
+    setTimeout(function(){
+        typewriter(i+1)
+    }, 100)
+}
+
+document.body.onload = function(){
+    setTimeout(function(){
+        typewriter(0)
+    }, 1500)
+}
